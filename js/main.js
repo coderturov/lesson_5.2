32af0c9d3fad857befbef6c959b7139659fb8fed
Vladimir ASU <vladimir_t1@mail.ru>
2018-11-26 14:56:16 +0500

    исправленное

---
+++ js/main.js
@@ -0,0 +1,45 @@
+let start = document.getElementById("start"),
+    budgetVal = document.getElementsByClassName("budget-value"),
+    dayBudgetVal = document.getElementsByClassName("daybudget-value"),
+    levelVal = document.getElementsByClassName("level-value"),
+    expenVal = document.getElementsByClassName("expenses-value"),
+    optExpenVal = document.getElementsByClassName("optionalexpenses-value"),
+    incomeVal = document.getElementsByClassName("income-value"),
+    monthsavVal = document.getElementsByClassName("monthsavings-value"),
+    yearsavVal = document.getElementsByClassName("yearsavings-value"),
+    inputExpen = document.getElementsByClassName("expenses-item"),
+    expItemBtn = document.getElementsByTagName("button")[0],
+    optExpBtn = document.getElementsByTagName("button")[1],
+    countBtn = document.getElementsByTagName("button")[2],
+    optExpItem = document.querySelectorAll(".optionalexpenses-item"),
+    chooseIncome = document.querySelectorAll(".choose-income"),
+    savings = document.querySelectorAll("#savings"),
+    chooseSum = document.querySelectorAll(".choose-sum"),
+    percent = document.querySelectorAll(".choose-percent"),
+    year = document.querySelectorAll(".year-value"),
+    month = document.querySelectorAll(".month-value"),
+    day = document.querySelectorAll(".day-value");
+
+
+
+    console.log(start);
+    console.log(budgetVal);
+    console.log(dayBudgetVal);
+    console.log(levelVal);
+    console.log(expenVal);
+    console.log(optExpenVal);
+    console.log(incomeVal);
+    console.log(monthsavVal);
+    console.log(yearsavVal);
+    console.log(inputExpen);
+    console.log(expItemBtn);
+    console.log(optExpBtn);
+    console.log(countBtn);
+    console.log(optExpItem);
+    console.log(chooseIncome);
+    console.log(savings);
+    console.log(chooseSum);
+    console.log(percent);
+    console.log(year);
+    console.log(month);
+    console.log(day);
